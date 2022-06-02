e6532dd46cf998913dd726dd7bb14b7466ee48da
Cesar Rojas Contreras <ocerojas@amazon.com>
2022-06-02 17:12:48 +0100

    alt is not gui

--- keyboards/3w6/rev2/keymaps/fieoner_pimoroni/keymap.c
+++ keyboards/3w6/rev2/keymaps/fieoner_pimoroni/keymap.c
@@ -92,8 +92,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     
 [_ALPHA_QWERTY] = LAYOUT(
         KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,                                                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  
-        KC_A,         KC_S,    KC_D,    KC_F,    KC_G,                                                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-        LSFT_T(KC_SLSH), KC_Z, KC_X,    KC_C,    KC_V,                                                   KC_B,    KC_N,    KC_M,    KC_COMM, RSFT_T(KC_DOT),
+KC_A,         KC_S,    KC_D,    KC_F,    KC_G,                                                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+LSFT_T(KC_SLSH), KC_Z, KC_X,    KC_C,    KC_V,                                                   KC_B,    KC_N,    KC_M,    KC_COMM, RSFT_T(KC_DOT),
             
                         LCTL_T(KC_ESC), LT(_NUM,KC_SPC), LT(_NAV, KC_TAB),     LT(_SYM, KC_BSPC), KC_ENT, LALT_T(KC_DEL)         
     ),
@@ -112,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NAV] = LAYOUT(
         XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX,                                XXXXXXX, KC_PGDN,   KC_UP, KC_PGUP,  KC_DEL,
         KC_MPRV, KC_MPLY, KC_MSTP, KC_MNXT, KC_CAPS,                                KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT,  KC_END,
-        KC_TAB, XXXXXXX, XXXXXXX, LALT_T(OSM(KC_LALT)), KC_LALT,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LSFT,
+        KC_TAB, XXXXXXX, XXXXXXX, LALT_T(OSM(KC_LALT)), KC_LGUI,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LSFT,
 	                                XXXXXXX, XXXXXXX, XXXXXXX,      _______, MO(_CFG), XXXXXXX
     ),
     [_NUM] = LAYOUT(
